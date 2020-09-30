a058678b8b4cdb92f342d699aae88eea8a50dd7e
Alexey Mikhailyuk <35634279+alexmihalyk23@users.noreply.github.com>
2020-09-30 19:58:28 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ parser.add_argument('-i', help='Image', dest='img', required=True)
 parser.add_argument('-t', help='Yolo text file', dest='txt',required=True)
 
 args = parser.parse_args()
-#напишите путь к нужному вам изображению
 frame = cv2.imread(args.img)
 coordinates = []
 with open(args.txt, "r") as lines:
@@ -18,7 +17,7 @@ with open(args.txt, "r") as lines:
 		print(x1,y1,w2,h2)
 		w, h = int(w2 * wT), int(h2 * hT)
 		x, y = int((x1 * wT) - w / 2), int((y1 * hT) - h / 2)
-# # 0 1 2 3 x1 - центр по x в процентах y1- центр по y в процентах w1- центр по w в процентах h1- центр по h в процентах
+# # 0 1 2 3 x1 - центр по x в процентах y1- центр по y в процентах w2- центр по w в процентах h2- центр по h в процентах
 		cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,255))
 		cv2.putText(frame,str(idx), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (128, 0, 255), 2)
 cv2.imshow("f", frame)
